2315deeddafcf348f2090b96e63daaad307339c9
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2021-08-06 10:15:25 +0530

    react comp

--- react-aug/src/App.js
+++ react-aug/src/App.js
@@ -1,12 +1,27 @@
-import React from "react";
-/* import './assets/css/one.css' */
+import Navbar from "./components/Navbar/Navbar";
 function App() {
   return (
     <div>
-      <h1>Welcome PSA</h1>
-      <button className="btn btn-success">Login</button>
+      <Navbar />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <div className="card">
+              <div className="card-header bg-primary">
+                <h4>Image Header</h4>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="card">
+              <div className="card-header">
+                <h4>Image Header</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
